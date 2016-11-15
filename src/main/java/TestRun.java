d9b3c1f4c394a276a60ebcac427e10ac8c70b643
sooglejay <394055524@qq.com>
2016-11-15 16:19:19 +0800

    first commit

---
+++ src/main/java/TestRun.java
@@ -0,0 +1,12 @@
+/**
+ * author : Jerry
+ **/
+
+/**
+ * Created by sooglejay on 16/11/14.
+ */
+public class TestRun {
+    public static void main(String[] args) {
+        System.out.print("123\n");
+    }
+}
